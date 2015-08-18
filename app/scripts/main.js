c41f63e6ae90cfc88164f84fa4f59c9f196ef626
Javier Enrique <s0wkete@gmail.com>
2015-08-18 19:12:46 -0300

    base html version and basic css

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+/*global $:false */
+'use strict';
+
+// Document ready
+$(function () {
+	// Init modal and tooltip for the app
+	$('[data-toggle="tooltip"]').tooltip({
+		placement: 'top',
+		html: 'true'
+	});
+});
