97971d43a5d35d149c81deefab960428e87d6d93
Javier Enrique <s0wkete@gmail.com>
2015-08-19 13:25:29 -0300

    Simple effect in animate.css for transition between form states

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,4 +8,15 @@ $(function () {
 		placement: 'top',
 		html: 'true'
 	});
+	// Animate the bag form
+	$('#form-bag .btn-go').on('click', function(){
+		$('#form-bag').addClass('hidden');
+		$('#form-login').removeClass('hidden');
+		$('#form-login').addClass('animated fadeIn');
+	});
+	$('#form-login .back-form').on('click', function(){
+		$('#form-login').addClass('hidden');
+		$('#form-bag').removeClass('hidden');
+		$('#form-bag').addClass('animated fadeIn');
+	});
 });
