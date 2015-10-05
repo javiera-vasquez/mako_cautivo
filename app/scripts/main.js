c6ed344c64951b1277a9c8d8cc1ac3b03620caf5
Javier Enrique <s0wkete@gmail.com>
2015-10-05 10:27:28 -0300

    changing modals

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,10 +4,13 @@
 // Document ready
 $(function () {
 	// Init modal and tooltip for the app
-	$('[data-toggle="popover"]').popover({
-		placement: 'top',
-		trigger: 'focus'
+	$('.total-pop').popover({
+		placement: 'bottom',
+		trigger: 'click',
+		title: 'Cuota de datos consumidos',
+		content: 'Es la suma de los GB que has consumido de tu Plan y de las Bolsas que hayas contratado.'
 	});
+
 	$('.modal-config-open').on('click', function(){
 		$('#config-modal').modal();
 	});
@@ -15,14 +18,27 @@ $(function () {
 		$('#purchase-modal').modal();
 	});
 	// Animate the bag form
-	$('#form-bag .btn-go').on('click', function(){
-		$('#form-bag').addClass('hidden');
-		$('#form-login').removeClass('hidden');
-		$('#form-login').addClass('animated fadeIn');
+	// $('#form-bag .btn-go').on('click', function(){
+	// 	$('#form-bag').addClass('hidden');
+	// 	$('#form-login').removeClass('hidden');
+	// 	$('#form-login').addClass('animated fadeIn');
+	// });
+	// $('#form-login .back-form').on('click', function(){
+	// 	$('#form-login').addClass('hidden');
+	// 	$('#form-bag').removeClass('hidden');
+	// 	$('#form-bag').addClass('animated fadeIn');
+	// });
+	// Resize and set width of a progress bar
+	$('.dinamic-gradient').each(function(){
+		var width = $(this).width();
+		$(this).children('.gradient').css('background-size', width);
 	});
-	$('#form-login .back-form').on('click', function(){
-		$('#form-login').addClass('hidden');
-		$('#form-bag').removeClass('hidden');
-		$('#form-bag').addClass('animated fadeIn');
+});
+
+// Document resize
+$(window).resize(function() {
+	$('.dinamic-gradient').each(function(){
+		var width = $(this).width();
+		$(this).children('.gradient').css('background-size', width);
 	});
 });
